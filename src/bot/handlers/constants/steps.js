af5cb478909d3c3796d476adfdfdaf9d49a6a413
Dima Minka <cdk@cdk.co.il>
2026-06-15 16:31:21 +0300

    refactor(bot): introduce strict step constants

---
+++ src/bot/handlers/constants/steps.js
@@ -0,0 +1,23 @@
+export const USERS_STEPS = {
+  SQUAD: 'squad',
+  SQUAD_TEXT: 'squad_text',
+  USERNAME: 'username',
+  CONFIRM: 'confirm',
+  USER: 'user',
+  SELECT_SQUAD: 'select_squad',
+};
+
+export const ROTATION_STEPS = {
+  DURATION: 'duration',
+  ALERT: 'alert',
+  STANDBY: 'standby',
+  REST: 'rest',
+  CONFIRM: 'confirm',
+  OVERWRITE: 'overwrite',
+};
+
+export const MISSION_STEPS = {
+  SELECT_ROTATION: 'select_rotation',
+  SELECT_DAY: 'select_day',
+  TIME_INPUT: 'time_input',
+};
